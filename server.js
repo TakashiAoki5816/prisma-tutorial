db6fba209ec9e8a816eaa42096bd105ed32d8df1
Takashi Aoki <arfaben_5816@icloud.com>
2024-02-04 22:00:07 +0900

    feat:post取得

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const PORT = 8000;
 const prisma = new PrismaClient();
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const posts = await prisma.posts.findMany();
+  return res.json(posts);
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const post = await prisma.posts.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.json(post);
+});
+
 app.post("/", async (req, res) => {
   const { title, body } = req.body;
   const posts = await prisma.posts.create({
