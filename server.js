f0080635fbe87cbb6a03e10679b742aebe599260
Takashi Aoki <arfaben_5816@icloud.com>
2024-02-04 21:42:36 +0900

    feat:post登録

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const PORT = 8000;
+
+const prisma = new PrismaClient();
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  const posts = await prisma.posts.create({
+    data: {
+      title: title,
+      body: body,
+    }
+  });
+
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log("サーバーが起動中...")
